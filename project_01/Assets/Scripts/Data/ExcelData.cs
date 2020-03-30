5d72f924893ceb15446d722398f035926c252bef
yamashiro_mondo <yamashiro@prolead.co.jp>
2020-03-30 17:23:09 +0900

    <追加>　犬の画像、ExcelData追加

---
+++ project_01/Assets/Scripts/Data/ExcelData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Data
+{
+    /// <summary>
+    /// Excelデータクラス
+    /// </summary>
+    public class ExcelData
+    {
+    }
+}
